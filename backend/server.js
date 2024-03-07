4f88343cb604a9cd7444b090960559412a6f455b
Samtech361 <nsamuel361@gmail.com>
2024-03-07 18:56:08 +0300

    api auth routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 // changed type: common js => type: module
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 dotenv.config();
 
 let PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  //root route https://localhost:5000/
-  res.send("welcome to homepage");
-});
+// middleware
+app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT, () => {
   console.log("listening on port " + PORT);
